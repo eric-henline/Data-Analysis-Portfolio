a322276c8233a642bbc8fea88f014ae5d6dc796f
Eric Henline <ehenline44@gmail.com>
2022-07-21 21:01:02 -0700

    Update query6.sql

--- Tableau&SQL Case Study AEMR Energy Utility/query6.sql
+++ Tableau&SQL Case Study AEMR Energy Utility/query6.sql
@@ -3,17 +3,17 @@
 
 SELECT
 	Count(*) as Total_Number_Outage_Events,
-  (Participant_Code),
-  Status,
-  Year(Start_Time) as Year
+  	(Participant_Code),
+  	Status,
+  	Year(Start_Time) as Year
 FROM
 	AEMR
 WHERE
 	Status='Approved'
 GROUP BY
 	(Participant_Code),
-  Status,
-  Year(Start_Time)
+  	Status,
+  	Year(Start_Time)
 ORDER BY 
 	Year(Start_Time),
-  (Participant_Code)
+  	(Participant_Code)
