f029215f3e56c8349dbab0d7bf3abe28a79c3b08
Eric Henline <ehenline44@gmail.com>
2022-07-21 20:48:25 -0700

    Update query2.sql

--- Tableau&SQL Case Study AEMR Energy Utility/query2.sql
+++ Tableau&SQL Case Study AEMR Energy Utility/query2.sql
@@ -3,19 +3,19 @@
 -- Please note: The average duration in days should be rounded to 2 decimal places for ease of comparison. Additionally, include the overall count of outage events per outage type
 
 SELECT
-	Status,
-  Reason,
-  Count(*) as Total_Number_Outage_Events,
-  ROUND(AVG((TIMESTAMPDIFF(MINUTE, Start_Time, End_Time)/60)/24),2) AS Average_Outage_Duration_Time_Days,
-  YEAR(Start_Time) as Year
+  	Status,
+  	Reason,
+  	COUNT(*) as Total_Number_Outage_Events,
+  	ROUND(AVG((TIMESTAMPDIFF(MINUTE, Start_Time, End_Time)/60)/24),2) AS Average_Outage_Duration_Time_Days,
+  	YEAR(Start_Time) as Year
 FROM
 	AEMR
 WHERE
 	Status='Approved'
 GROUP BY
 	Status,
-  Reason,
-  YEAR(Start_Time)
+  	Reason,
+  	YEAR(Start_Time)
 ORDER BY
-   YEAR(Start_Time),
-   Reason
+   	YEAR(Start_Time),
+   	Reason
