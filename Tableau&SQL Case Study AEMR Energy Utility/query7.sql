272fe995dd8fba2cf3deea3a144f14044997ddd7
Eric Henline <ehenline44@gmail.com>
2022-07-21 21:03:00 -0700

    Update query7.sql

--- Tableau&SQL Case Study AEMR Energy Utility/query7.sql
+++ Tableau&SQL Case Study AEMR Energy Utility/query7.sql
@@ -3,17 +3,17 @@
 
 SELECT
 	Participant_Code,
-  Status,
-  Year(Start_Time) as Year,
-  ROUND(AVG((TIMESTAMPDIFF(MINUTE, Start_Time, End_Time)/60)/24),2) AS Average_Outage_Duration_Time_Days
+  	Status,
+  	Year(Start_Time) as Year,
+  	ROUND(AVG((TIMESTAMPDIFF(MINUTE, Start_Time, End_Time)/60)/24),2) AS Average_Outage_Duration_Time_Days
 FROM
 	AEMR
 WHERE
 	Status='Approved'
 GROUP BY
 	Participant_Code,
-  Status,
-  Year(Start_Time)
+  	Status,
+  	Year(Start_Time)
 ORDER BY 
 	Year(Start_Time),
-  CAST(Avg(CAST(TIMESTAMPDIFF(DAY,Start_Time,End_Time)AS DECIMAL(18,2))) AS DECIMAL(18,2)) DESC
+  	CAST(Avg(CAST(TIMESTAMPDIFF(DAY,Start_Time,End_Time)AS DECIMAL(18,2))) AS DECIMAL(18,2)) DESC
