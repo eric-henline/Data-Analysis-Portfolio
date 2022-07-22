b37626d8fd3428a99989783f83fc408ba47b4b1c
Eric Henline <ehenline44@gmail.com>
2022-07-21 20:56:29 -0700

    Update query4.sql

--- Tableau&SQL Case Study AEMR Energy Utility/query4.sql
+++ Tableau&SQL Case Study AEMR Energy Utility/query4.sql
@@ -2,19 +2,19 @@
 -- Order by Reason and Month.
 
 SELECT
-    Status,
-    Reason,
-    Count(*) as Total_Number_Outage_Events,
-    Month(Start_Time) as Month
+    	Status,
+    	Reason,
+    	Count(*) as Total_Number_Outage_Events,
+    	Month(Start_Time) as Month
 FROM
-	  AEMR
+	AEMR
 WHERE
-	  Status='Approved'
-	  AND YEAR(Start_Time) = 2017
+	Status='Approved'
+	AND YEAR(Start_Time) = 2017
 GROUP BY
-	  Status,
-    Reason,
-    Month(Start_Time)
+	Status,
+    	Reason,
+    	Month(Start_Time)
 ORDER BY
-	  Reason,
-    Month
+	Reason,
+    	Month
