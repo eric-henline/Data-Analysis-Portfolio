b65e7eddb0e6af71e274313fe1a656ae9b04ca6a
Eric Henline <ehenline44@gmail.com>
2022-07-21 21:14:18 -0700

    Update 9query.sql

--- Tableau&SQL Case Study AEMR Energy Utility/9query.sql
+++ Tableau&SQL Case Study AEMR Energy Utility/9query.sql
@@ -3,18 +3,18 @@
 
 SELECT 
 	Status,
-  Reason,
-  Year(Start_Time) AS Year,
-  ROUND(AVG(Outage_MW),2) AS Avg_Outage_MW_Loss,
-  Cast(ROUND(AVG(Cast(TIMESTAMPDIFF(MINUTE, Start_Time, End_Time) AS DECIMAL(18,2))),2) AS DECIMAL(18,2)) AS Average_Outage_Duration_Time_Minutes
+  	Reason,
+  	Year(Start_Time) AS Year,
+  	ROUND(AVG(Outage_MW),2) AS Avg_Outage_MW_Loss,
+  	Cast(ROUND(AVG(Cast(TIMESTAMPDIFF(MINUTE, Start_Time, End_Time) AS DECIMAL(18,2))),2) AS DECIMAL(18,2)) AS Average_Outage_Duration_Time_Minutes
 FROM 
 	AEMR
 WHERE 
 	Status='Approved' 
 GROUP BY 
 	Status,
-  Reason,
-  Year(Start_Time)
+  	Reason,
+  	Year(Start_Time)
 ORDER BY 
 	Year(Start_Time),
-  Reason
+  	Reason
